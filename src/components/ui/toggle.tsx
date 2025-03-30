d1cd6190e665f35b68c531870e8a4e9534a6187a
Max Lasitsa <mlasitsa@css.edu>
2025-03-29 20:10:31 -0700

    setting up UI

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -1,12 +1,28 @@
 // import React from 'react'
 
-const Toggle = ({buttonName} : {buttonName: string}) => {
+const Toggle = ({buttonName, description} : {buttonName: string, description?: string}) => {
   return (
     <>
-    <label className='gap-x-6'> 
-        <input type="radio" name={buttonName} value="option1"></input>
-        {buttonName}
-    </label>
+    <div>
+      <label
+        htmlFor={buttonName}
+        className="flex items-center justify-between gap-4 rounded border border-gray-300 bg-white p-3 text-sm font-medium shadow-sm transition-colors hover:bg-gray-50 has-checked:border-blue-600 has-checked:ring-1 has-checked:ring-blue-600"
+      >
+        <div>
+          <p className="text-gray-900">{buttonName}</p>
+          <p className="text-gray-700">{description? description : ""}</p>
+        </div>
+
+        <input
+          type="radio"
+          name={buttonName}
+          value={buttonName}
+          id={buttonName}
+          className="size-5 border-gray-300"
+          checked
+        />
+      </label>
+    </div>
     </>
   )
 }
